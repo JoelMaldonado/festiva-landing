b92d9dcf969e604e0ed3f04a1ccccd0861028df0
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2025-06-21 00:48:56 -0500

    feat: Añadir animación de marquee en el componente de contacto para mejorar la interactividad

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue}"],
   theme: {
     extend: {
+      animation: {
+        marquee: "marquee 20s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
+      },
       colors: {
         p1: "#FF4081", // rosa Festiva
         dark: "#0D0D0D",
